09422a6fa3e97e4d59b489e8f6a1d16570f71bfb
yxiaoGit <yxabc@yahoo.com>
2019-12-31 16:14:29 -0500

    Add files via upload

--- src/main/java/com/example/springeurekaclientsideloadbalancing/SpringEurekaClientSideLoadBalancingApplication.java
+++ src/main/java/com/example/springeurekaclientsideloadbalancing/SpringEurekaClientSideLoadBalancingApplication.java
@@ -2,15 +2,18 @@ package com.example.springeurekaclientsideloadbalancing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 @SpringBootApplication
+@EnableFeignClients
 public class SpringEurekaClientSideLoadBalancingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEurekaClientSideLoadBalancingApplication.class, args);
 	}
+	/*
 	@Bean
 	public  Client  client()
 	{
 		return  new Client();
-	}
+	} */
 }
